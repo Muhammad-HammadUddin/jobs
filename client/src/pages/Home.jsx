e9765825fc630b1fe66d9eaac7f01688649490c2
Muhammad Hammad Uddin <hammaduddin083@gmail.com>
2026-01-22 02:18:39 +0500

    fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
+import Footer from '../components/Footer'
+
 import AppDownload from '../components/AppDownload'
-import Footer from '../components/footer'
+
 
 const Home = () => {
   return (
